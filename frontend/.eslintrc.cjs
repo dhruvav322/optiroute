4f2ba4b96665771cdc55d1a6a511cbdd8fb21fc0
Dhruvav <dhruvavinay1809@gmail.com>
2025-11-19 15:01:46 +0530

    fix: simplify ESLint config to work without react plugin

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,23 +1,20 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'react/prop-types': 'off', // Using PropTypes but not requiring it for all components
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
 }
 
